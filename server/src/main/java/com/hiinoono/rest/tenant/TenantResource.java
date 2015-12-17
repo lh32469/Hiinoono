c3bc21c6fd4d90085bb0754f6374cca3b284eaab
lyle.harris@oracle.com <lyle.harris@oracle.com>
2015-12-16 16:26:21 -0800

    Fleshing out TenantResource a bit more

--- server/src/main/java/com/hiinoono/rest/tenant/TenantResource.java
+++ server/src/main/java/com/hiinoono/rest/tenant/TenantResource.java
@@ -1,15 +1,17 @@
 package com.hiinoono.rest.tenant;
 
 import com.hiinoono.jaxb.Tenant;
+import com.hiinoono.jaxb.Tenants;
 import com.hiinoono.jaxb.User;
 import com.hiinoono.rest.zk.ZooKeeperResource;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import org.slf4j.LoggerFactory;
 
 
@@ -30,7 +32,8 @@ public class TenantResource {
 
 
     @GET
-    public List<Tenant> getTenants() {
+    @Path("list")
+    public List<Tenant> getTenantsAsList() {
 
         // Clear passwords before sending
         return zkr.getTenants().map((tenant) -> {
@@ -41,6 +44,23 @@ public class TenantResource {
             return tenant;
         }).collect(Collectors.toList());
 
+    }
+
+
+    @GET
+    public Tenants getTenants() {
+
+        Tenants t = new Tenants();
+        t.getTenants().addAll(getTenantsAsList());
+        return t;
+
+    }
+
+
+    @GET
+    @Path("{name}")
+    public Tenant getTenant(@PathParam("name") String name) {
+        return zkr.getTenantByName(name);
     }
 
 
--- server/src/main/java/com/hiinoono/rest/zk/ZooKeeperResource.java
+++ server/src/main/java/com/hiinoono/rest/zk/ZooKeeperResource.java
@@ -4,9 +4,11 @@ import com.hiinoono.jaxb.Tenant;
 import com.hiinoono.jaxb.User;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 import javax.inject.Singleton;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 
 /**
@@ -40,16 +42,38 @@ public class ZooKeeperResource {
 
         t1.setAdmin(admin);
         tenants.add(t1);
-
+        
         User u1 = new User();
         u1.setName("Hiinoono Sample User");
         u1.setEmail("sample@hinoono.com");
         u1.setPassword("welcome1");
 
         t1.getUsers().add(u1);
+        t1.getUsers().add(u1);
 
+        Tenant t2 = new Tenant();
+        t2.setName("Hiinoono-2");
+        admin = new User();
+        admin.setName("Hiinoono-2 Administrator");
+        admin.setEmail("hiinoono-2@hinoono.com");
+        admin.setPassword("welcome1");
+        t2.setAdmin(admin);
+
+        tenants.add(t2);
+        
         return tenants.stream();
     }
 
+    
+     public Tenant getTenantByName( String name) {
+         
+        // Filter sample data
+         
+        Optional<Tenant> tenant
+                = getTenants().filter(
+                        t -> t.getName().equals(name)).findFirst();
+        
+        return tenant.orElse(null);
+     }
 
 }
