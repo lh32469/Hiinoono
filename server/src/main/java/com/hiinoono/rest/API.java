dea2e5ba30822bcf8d1a82e2e9dbbff353b9842c
lyle.harris@oracle.com <lyle.harris@oracle.com>
2015-12-28 08:46:48 -0800

    Added ZooKeeper persistence option, UserResource and TimingFilter

--- server/src/main/java/com/hiinoono/rest/API.java
+++ server/src/main/java/com/hiinoono/rest/API.java
@@ -1,8 +1,9 @@
 package com.hiinoono.rest;
 
-import com.hiinoono.rest.exceptions.HiinoonoExceptionMapper;
 import com.hiinoono.persistence.PersistenceManager;
 import com.hiinoono.persistence.UnitTestPersistenceManager;
+import com.hiinoono.persistence.zk.ZooKeeperPersistenceManager;
+import com.hiinoono.persistence.zk.ZooKeeperClient;
 import com.hiinoono.rest.auth.AuthorizationFilter;
 import com.hiinoono.rest.auth.HiinoonoRolesFeature;
 import com.hiinoono.rest.exceptions.ClientErrorExceptionMapper;
@@ -10,12 +11,13 @@ import com.hiinoono.rest.exceptions.DefaultExceptionMapper;
 import com.hiinoono.rest.node.NodeResource;
 import com.hiinoono.rest.site.SiteResource;
 import com.hiinoono.rest.tenant.TenantResource;
+import com.hiinoono.rest.user.UserResource;
+import java.io.IOException;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.message.MessageProperties;
 import org.glassfish.jersey.moxy.json.MoxyJsonConfig;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.glassfish.jersey.server.wadl.WadlFeature;
 
 
@@ -30,6 +32,7 @@ public class API extends ResourceConfig {
         register(SiteResource.class);
         register(NodeResource.class);
         register(TenantResource.class);
+        register(UserResource.class);
         //register(RolesAllowedDynamicFeature.class);
         register(HiinoonoRolesFeature.class);
         register(AuthorizationFilter.class);
@@ -43,6 +46,7 @@ public class API extends ResourceConfig {
             register(LoggingFilter.class);
         }
         register(WadlFeature.class);
+        register(TimingFilter.class);
 
         MoxyJsonConfig config = new MoxyJsonConfig();
         config.setFormattedOutput(true);
@@ -60,7 +64,21 @@ public class API extends ResourceConfig {
 
         @Override
         protected void configure() {
-            bind(new UnitTestPersistenceManager()).to(PersistenceManager.class);
+
+            String zooKeepers = System.getProperty("zooKeepers");
+
+            if (zooKeepers == null) {
+                bind(new UnitTestPersistenceManager()).to(PersistenceManager.class);
+            } else {
+                try {
+                    bind(new ZooKeeperClient(zooKeepers,"Welcome1"));
+                    bindAsContract(ZooKeeperPersistenceManager.class).to(PersistenceManager.class);
+                } catch (IOException ex) {
+                    System.err.println(ex.getLocalizedMessage());
+                    System.exit(1);
+                }
+            }
+
         }
 
 
