350fd5685cd967aa76ad392f1dc2ead3610d5141
lyle.harris@oracle.com <lyle.harris@oracle.com>
2016-01-17 15:34:18 -0800

    Place container in transition state while creating

--- server/src/main/java/com/hiinoono/os/container/ContainerCreator.java
+++ server/src/main/java/com/hiinoono/os/container/ContainerCreator.java
@@ -3,25 +3,16 @@ package com.hiinoono.os.container;
 import com.hiinoono.Utils;
 import com.hiinoono.jaxb.Container;
 import com.hiinoono.jaxb.State;
-import com.hiinoono.persistence.zk.GetTenantByName;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.netflix.hystrix.HystrixCommandProperties;
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.ACL;
-import org.eclipse.persistence.jaxb.JAXBContextFactory;
 import org.slf4j.LoggerFactory;
 
 
@@ -36,6 +27,11 @@ public class ContainerCreator extends HystrixCommand<Container> {
     private final Container container;
 
     private final ZooKeeper zk;
+
+    /**
+     * Path to transition state for this Container.
+     */
+    private final String transitionState;
 
     /**
      * ACL to create nodes with.
@@ -57,28 +53,18 @@ public class ContainerCreator extends HystrixCommand<Container> {
             = LoggerFactory.getLogger(ContainerCreator.class);
 
 
-    static {
-
-        Class[] classes = {Container.class};
-
-        try {
-            Map<String, Object> properties = new HashMap<>();
-            properties.put("eclipselink.media-type", "application/json");
-            jc = JAXBContextFactory.createContext(classes, properties);
-
-        } catch (JAXBException ex) {
-            LOG.error(ex.getErrorCode(), ex);
-        }
-
-    }
-
-
     public ContainerCreator(Container container, ZooKeeper zk) {
         super(Setter
                 .withGroupKey(GROUP_KEY)
                 .andCommandPropertiesDefaults(COMMAND_PROPS));
+        
         this.container = container;
         this.zk = zk;
+
+        transitionState = ContainerConstants.CONTAINERS
+                + "/" + Utils.getNodeId()
+                + ContainerConstants.TRANSITIONING
+                + "/" + container.getName();
 
     }
 
@@ -87,17 +73,24 @@ public class ContainerCreator extends HystrixCommand<Container> {
     protected Container run() throws Exception {
         LOG.info("Creating: " + container.getName());
 
+        container.setState(State.CREATING);
+        // Add to /containers/{nodeId}/transition
+        ContainerUtils.marshall(zk, container, transitionState);
+
         // Simulate creation
         Thread.sleep(15000);
         container.setState(State.CREATED);
 
+        // Delete transition state
+        zk.delete(transitionState, -1);
+
         // Create and move to /containers/{nodeId}/created
-        final String path = ContainerConstants.CONTAINERS
+        final String created = ContainerConstants.CONTAINERS
                 + "/" + Utils.getNodeId()
                 + ContainerConstants.CREATED
                 + "/" + container.getName();
 
-        ContainerUtils.marshall(zk, container, path);
+        ContainerUtils.marshall(zk, container, created);
 
         LOG.info("Created " + container.getName());
         return container;
@@ -117,7 +110,12 @@ public class ContainerCreator extends HystrixCommand<Container> {
                 + "/" + container.getName();
 
         try {
+
+            // Delete transition state
+            zk.delete(transitionState, -1);
+
             ContainerUtils.marshall(zk, container, path);
+
         } catch (JAXBException | KeeperException | InterruptedException ex) {
             LOG.error(ex.getLocalizedMessage(), ex);
         }
