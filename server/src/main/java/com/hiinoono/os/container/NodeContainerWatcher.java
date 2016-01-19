87332cba4d38986264a8460999839bd88463b2a4
lyle.harris@oracle.com <lyle.harris@oracle.com>
2016-01-19 10:54:31 -0800

    Register Node on startup

--- server/src/main/java/com/hiinoono/os/container/NodeContainerWatcher.java
+++ server/src/main/java/com/hiinoono/os/container/NodeContainerWatcher.java
@@ -2,10 +2,16 @@ package com.hiinoono.os.container;
 
 import com.hiinoono.Utils;
 import com.hiinoono.jaxb.Container;
+import com.hiinoono.jaxb.Node;
+import com.hiinoono.os.ShellCommand;
 import static com.hiinoono.os.container.ContainerConstants.CONTAINERS;
 import static com.hiinoono.os.container.ContainerConstants.NEW;
 import static com.hiinoono.os.container.ContainerConstants.STATES;
+import com.hiinoono.persistence.zk.ZKUtils;
 import com.hiinoono.persistence.zk.ZooKeeperClient;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,11 +19,14 @@ import java.util.List;
 import java.util.Map;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.ZKUtil;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.ACL;
@@ -33,12 +42,15 @@ import org.slf4j.LoggerFactory;
  */
 public class NodeContainerWatcher implements Watcher, ContainerConstants {
 
-    private static JAXBContext jc;
+    /**
+     * Top-level ZK path/dir for Node registration.
+     */
+    public static final String NODES = "/nodes";
 
     /**
-     * ZK path associated with this Node.
+     * ZK path associated with this Node in the /containers path/dir.
      */
-    private final String nodePath;
+    private final String containerNodePath;
 
     /**
      * ACL to create nodes with.
@@ -47,7 +59,7 @@ public class NodeContainerWatcher implements Watcher, ContainerConstants {
      *
      * For development: Ids.OPEN_ACL_UNSAFE
      */
-    private final ArrayList<ACL> acl = ZooDefs.Ids.OPEN_ACL_UNSAFE;
+    private final static ArrayList<ACL> acl = ZooDefs.Ids.OPEN_ACL_UNSAFE;
 
     /**
      * ZooKeeper (zk) cannot be a field in case of session timeouts so should be
@@ -60,29 +72,30 @@ public class NodeContainerWatcher implements Watcher, ContainerConstants {
             = LoggerFactory.getLogger(NodeContainerWatcher.class);
 
 
-    static {
-
-        Class[] classes = {Container.class};
-
-        try {
-            Map<String, Object> properties = new HashMap<>();
-            properties.put("eclipselink.media-type", "application/json");
-            jc = JAXBContextFactory.createContext(classes, properties);
-
-        } catch (JAXBException ex) {
-            LOG.error(ex.getErrorCode(), ex);
-        }
-
-    }
-
-
     public NodeContainerWatcher(ZooKeeperClient zkc) throws
-            InterruptedException, KeeperException {
+            InterruptedException, KeeperException,
+            IOException, JAXBException, GeneralSecurityException {
 
         this.zooKeeperClient = zkc;
         ZooKeeper zk = zooKeeperClient.getZookeeper();
 
-        this.nodePath = CONTAINERS + "/" + Utils.getNodeId();
+        if (zk.exists(NODES, null) == null) {
+            LOG.info("Creating: " + NODES);
+            zk.create(NODES, "Initialized".getBytes(),
+                    acl, CreateMode.PERSISTENT);
+        }
+
+        Node node = new Node();
+        node.setId(Utils.getNodeId());
+        node.setJoined(Utils.now());
+        ShellCommand hostname = new ShellCommand("hostname");
+        node.setHostname(hostname.execute());
+
+        final String nodeStatus = NODES + "/" + Utils.getNodeId();
+        if (zk.exists(nodeStatus, null) != null) {
+            zk.delete(nodeStatus, -1);
+        }
+        ZKUtils.saveEphemeral(zk, node, nodeStatus);
 
         if (zk.exists(CONTAINERS, null) == null) {
             LOG.info("Creating: " + CONTAINERS);
@@ -91,15 +104,16 @@ public class NodeContainerWatcher implements Watcher, ContainerConstants {
         }
 
         // Create path for this Node
-        if (zk.exists(nodePath, null) == null) {
-            LOG.info("Creating: " + nodePath);
-            zk.create(nodePath, "Initialized".getBytes(),
+        containerNodePath = CONTAINERS + "/" + Utils.getNodeId();
+        if (zk.exists(containerNodePath, null) == null) {
+            LOG.info("Creating: " + containerNodePath);
+            zk.create(containerNodePath, "Initialized".getBytes(),
                     acl, CreateMode.PERSISTENT);
         }
 
         // Make sure the state path/dirs exist
         for (String state : STATES) {
-            final String path = nodePath + state;
+            final String path = containerNodePath + state;
             if (zk.exists(path, null) == null) {
                 LOG.info("Creating: " + path);
                 zk.create(path, "Initialized".getBytes(),
@@ -109,7 +123,7 @@ public class NodeContainerWatcher implements Watcher, ContainerConstants {
 
         // Start watching the state paths/dirs
         for (String state : STATES) {
-            zk.getChildren(nodePath + state, this);
+            zk.getChildren(containerNodePath + state, this);
         }
 
     }
@@ -134,12 +148,12 @@ public class NodeContainerWatcher implements Watcher, ContainerConstants {
                 for (String c : containers) {
 
                     final String cPath = event.getPath() + "/" + c;
-                    Container container = ContainerUtils.load(zk, cPath);
+                    Container container = ZKUtils.loadContainer(zk, path);
 
-                    if (path.equals(nodePath + NEW)) {
+                    if (path.equals(containerNodePath + NEW)) {
                         new ContainerCreator(container, zk).queue();
                         zk.delete(cPath, -1);
-                    } else if (path.equals(nodePath + CREATED)) {
+                    } else if (path.equals(containerNodePath + CREATED)) {
                         new ContainerStarter(container, zk).queue();
                         zk.delete(cPath, -1);
                     }
