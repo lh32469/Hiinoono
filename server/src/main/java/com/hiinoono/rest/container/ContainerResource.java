13af85244960109eed3115bc44efbfdaa49b642b
lyle.harris@oracle.com <lyle.harris@oracle.com>
2016-01-19 16:39:57 -0800

    Implemented get container functionality

--- server/src/main/java/com/hiinoono/rest/container/ContainerResource.java
+++ server/src/main/java/com/hiinoono/rest/container/ContainerResource.java
@@ -9,13 +9,16 @@ import com.hiinoono.persistence.PersistenceManager;
 import com.hiinoono.rest.auth.HiinoonoRolesAllowed;
 import com.hiinoono.rest.auth.Roles;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotAcceptableException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
@@ -118,6 +121,68 @@ public class ContainerResource {
         containers.getContainers().addAll(list);
 
         return containers;
+    }
+
+
+    /**
+     * Users can only get the containers they created whereas the Tenant Admin
+     * can get containers for all users in the tenancy and Hiinoono Admin can
+     * get any container.
+     *
+     * @param tenantParam Name of the Tenant who owns the Container.
+     * @param userParam Name of the User who owns the Container.
+     * @param containerName Name of the Container to get.
+     *
+     * @return
+     */
+    @GET
+    @Path("get/{tenant}/{user}/{name}")
+    @HiinoonoRolesAllowed(roles = {Roles.H_ADMIN, Roles.T_ADMIN, Roles.USER},
+            message = "You are not permitted to get a container")
+    public Container get(@PathParam("tenant") String tenantParam,
+            @PathParam("user") String userParam,
+            @PathParam("name") String containerName) {
+
+        // Principal name is tenant/user
+        String principalName = sc.getUserPrincipal().getName();
+        LOG.trace("Principal Name: " + principalName);
+
+        final String tenantName = principalName.split("/")[0];
+        final String userName = principalName.split("/")[1];
+
+        List<Container> list;
+
+        if (sc.isUserInRole(Roles.H_ADMIN)) {
+            list = pm.getContainers().filter(
+                    n -> n.getOwner().getTenant().equals(tenantParam)
+                    && n.getOwner().getName().equals(userParam)
+                    && n.getName().equals(containerName)
+            ).collect(Collectors.toList());
+
+        } else if (sc.isUserInRole(Roles.T_ADMIN)) {
+            // Ignore Tenant PathParam, can only view in own tenancy
+            list = pm.getContainers().filter(
+                    n -> n.getOwner().getTenant().equals(tenantName)
+                    && n.getOwner().getName().equals(userParam)
+                    && n.getName().equals(containerName)
+            ).collect(Collectors.toList());
+
+        } else {
+            // Ignore Tenant and User PathParams, can only view own containers.
+            list = pm.getContainers().filter(
+                    n -> n.getOwner().getTenant().equals(tenantName)
+                    && n.getOwner().getName().equals(userName)
+                    && n.getName().equals(containerName)
+            ).collect(Collectors.toList());
+
+        }
+
+        if (list.isEmpty()) {
+            throw new NotAcceptableException("Container " + containerName
+                    + " doesn't exist.");
+        }
+
+        return list.get(0);
     }
 
 
