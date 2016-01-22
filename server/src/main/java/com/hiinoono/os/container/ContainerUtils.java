0b3aa7606af176afdde39671b64b5c4ceed3818e
lyle.harris@oracle.com <lyle.harris@oracle.com>
2016-01-21 16:28:52 -0800

    Re-arranged container ZK name to cn-name.user.tenant

--- server/src/main/java/com/hiinoono/os/container/ContainerUtils.java
+++ server/src/main/java/com/hiinoono/os/container/ContainerUtils.java
@@ -1,25 +1,7 @@
 package com.hiinoono.os.container;
 
-import com.hiinoono.Utils;
 import com.hiinoono.jaxb.Container;
 import com.hiinoono.jaxb.User;
-import com.hiinoono.persistence.zk.ZKUtils;
-import java.io.ByteArrayOutputStream;
-import java.io.StringReader;
-import java.security.GeneralSecurityException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.ZooDefs;
-import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.ACL;
-import org.eclipse.persistence.jaxb.JAXBContextFactory;
 import org.slf4j.LoggerFactory;
 
 
@@ -37,6 +19,8 @@ public class ContainerUtils {
      * Get a ZooKeeper friendly, unique Container name made up of Tenant name
      * plus User name plus actual container name.
      *
+     * name.user.tenant
+     *
      * @param container The Container to get name for.
      * @return ZK friendly name base on Container name and Owner.
      */
@@ -45,9 +29,10 @@ public class ContainerUtils {
         User owner = container.getOwner();
         zkName.append(container.getName());
         zkName.append(".");
-        zkName.append(owner.getTenant().replaceAll(" ", "_"));
-        zkName.append(".");
         zkName.append(owner.getName().replaceAll(" ", "_"));
+        zkName.append(".");
+        zkName.append(owner.getTenant().replaceAll(" ", "_"));
+
         return zkName.toString();
     }
 
