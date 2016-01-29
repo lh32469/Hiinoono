a079057b35c051e6b7c174cc9f02b45a1eb46c53
lyle.harris@oracle.com <lyle.harris@oracle.com>
2016-01-29 15:21:39 -0800

    Include volume group space available in node comparison

--- server/src/main/java/com/hiinoono/rest/node/NodeComparator.java
+++ server/src/main/java/com/hiinoono/rest/node/NodeComparator.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
 /**
  * Used for sorting Nodes for Container placement. Container to be placed is
  * passed in to be used for possibly placing on the same or different Node as
- * other Containers according to placement requirements. 
+ * other Containers according to placement requirements.
  *
  * @author Lyle T Harris
  */
@@ -22,15 +22,49 @@ public class NodeComparator implements Comparator<Node> {
     }
 
 
+    /**
+     * Compares its two arguments for order. Returns a negative integer, zero,
+     * or a positive integer as the first argument is less than, equal to, or
+     * greater than the second.<p>
+     *
+     * Compares two Nodes for order. Returns a negative integer if the first
+     * Node is better suited for placement than the second Node.
+     */
     @Override
     public int compare(Node n1, Node n2) {
 
-        // Initially just go with most available memory.
-        // TODO: Include previously allocated memory to containers.
-        Long n1Mem = n1.getMemAvailable();
-        Long n2Mem = n2.getMemAvailable();
+        int result = 0;
 
-        return (int) (n2Mem - n1Mem);
+        // Memory available is reported in KBytes so only
+        // care if one node has 100Meg more than the other.
+        long memDiffInMeg
+                = (n2.getMemAvailable() - n1.getMemAvailable()) / 1024;
+
+        if (memDiffInMeg < -100) {
+            result--;
+        } else if (memDiffInMeg > 100) {
+            result++;
+        }
+
+        // Gets another point if the memory difference is greater than 1Gig
+        // so will outweigh disk differences.
+        if (memDiffInMeg < -1000) {
+            result--;
+        } else if (memDiffInMeg > 1000) {
+            result++;
+        }
+
+        // Memory available is reported in Megabytes so only
+        // care if one node has 1Gig more than the other.
+        float diskDiffInGig = (n2.getVgFree() - n1.getVgFree()) / 1024;
+
+        if (diskDiffInGig < -1) {
+            result--;
+        } else if (diskDiffInGig > 1) {
+            result++;
+        }
+
+        return result;
 
     }
 
--- server/src/test/java/com/hiinoono/rest/node/NodeComparatorTest.java
+++ server/src/test/java/com/hiinoono/rest/node/NodeComparatorTest.java
@@ -49,23 +49,23 @@ public class NodeComparatorTest {
 
 
     /**
-     * Test of compare method, of class NodeComparator.
+     *
      */
     @Test
-    public void testCompare() {
-        System.out.println("compare");
+    public void smallMemoryDifference() {
+        System.out.println("smallMemoryDifference");
 
         Node n1 = new Node();
         n1.setHostname("node1");
-        n1.setMemAvailable(16000l);
+        n1.setMemAvailable(16000000l);
 
         Node n2 = new Node();
         n2.setHostname("node2");
-        n2.setMemAvailable(24000l);
+        n2.setMemAvailable(16005000l);
 
         Node n3 = new Node();
         n3.setHostname("node3");
-        n3.setMemAvailable(8000l);
+        n3.setMemAvailable(16000000l);
 
         List<Node> nodes = new LinkedList<>();
         nodes.add(n1);
@@ -75,8 +75,126 @@ public class NodeComparatorTest {
         Collections.sort(nodes, new NodeComparator(new Container()));
         System.out.println("1st: " + nodes.get(0).getHostname());
 
-        // Most memory wins for now.
+        for (Node node : nodes) {
+            System.out.println(node.getHostname());
+        }
+
+        assertEquals(n1, nodes.get(0));
+        assertEquals(n2, nodes.get(1));
+        assertEquals(n3, nodes.get(2));
+    }
+
+
+    /**
+     *
+     */
+    @Test
+    public void largeMemoryDifference() {
+        System.out.println("largeMemoryDifference");
+
+        Node n1 = new Node();
+        n1.setHostname("node1");
+        n1.setMemAvailable(16000000l);
+
+        Node n2 = new Node();
+        n2.setHostname("node2");
+        n2.setMemAvailable(24000000l);
+
+        Node n3 = new Node();
+        n3.setHostname("node3");
+        n3.setMemAvailable(8000000l);
+
+        List<Node> nodes = new LinkedList<>();
+        nodes.add(n1);
+        nodes.add(n2);
+        nodes.add(n3);
+
+        Collections.sort(nodes, new NodeComparator(new Container()));
+        System.out.println("1st: " + nodes.get(0).getHostname());
+
+        for (Node node : nodes) {
+            System.out.println(node.getHostname());
+        }
         assertEquals(n2, nodes.get(0));
+        assertEquals(n1, nodes.get(1));
+        assertEquals(n3, nodes.get(2));
+    }
+
+
+    /**
+     * Test that small differences in VG size doesn't affect sorting.
+     */
+    @Test
+    public void smallVolumeGroupDifference() {
+        System.out.println("smallVolumeGroupDifference");
+
+        Node n1 = new Node();
+        n1.setHostname("node1");
+        n1.setMemAvailable(128000l);
+        n1.setVgFree(127950.0f);
+
+        Node n2 = new Node();
+        n2.setHostname("node2");
+        n2.setMemAvailable(128000l);
+        n2.setVgFree(128000.0f);
+
+        Node n3 = new Node();
+        n3.setHostname("node3");
+        n3.setMemAvailable(128000l);
+        n3.setVgFree(128050.0f);
+
+        List<Node> nodes = new LinkedList<>();
+        nodes.add(n1);
+        nodes.add(n2);
+        nodes.add(n3);
+
+        Collections.sort(nodes, new NodeComparator(new Container()));
+        System.out.println("1st: " + nodes.get(0).getHostname());
+
+        for (Node node : nodes) {
+            System.out.println(node.getHostname());
+        }
+
+        assertEquals(n1, nodes.get(0));
+        assertEquals(n2, nodes.get(1));
+        assertEquals(n3, nodes.get(2));
+    }
+
+
+    /**
+     * Test that large differences in VG size does affect sorting.
+     */
+    @Test
+    public void largeVolumeGroupDifference() {
+        System.out.println("largeVolumeGroupDifference");
+
+        Node n1 = new Node();
+        n1.setHostname("node1");
+        n1.setVgFree(125000.0f);
+
+        Node n2 = new Node();
+        n2.setHostname("node2");
+        n2.setVgFree(128000.0f);
+
+        Node n3 = new Node();
+        n3.setHostname("node3");
+        n3.setVgFree(26000.0f);
+
+        List<Node> nodes = new LinkedList<>();
+        nodes.add(n1);
+        nodes.add(n2);
+        nodes.add(n3);
+
+        Collections.sort(nodes, new NodeComparator(new Container()));
+        System.out.println("1st: " + nodes.get(0).getHostname());
+
+        for (Node node : nodes) {
+            System.out.println(node.getHostname());
+        }
+
+        assertEquals(n2, nodes.get(0));
+        assertEquals(n1, nodes.get(1));
+        assertEquals(n3, nodes.get(2));
     }
 
 
