22a3310657fc2fa005ac008dfe5f4ff697fc76d4
lyle.harris@oracle.com <lyle.harris@oracle.com>
2016-02-12 15:54:41 -0800

    Sort Nodes returned by placement preference and added method for listing Managers as well.

--- server/src/main/java/com/hiinoono/rest/node/NodeResource.java
+++ server/src/main/java/com/hiinoono/rest/node/NodeResource.java
@@ -1,11 +1,17 @@
 package com.hiinoono.rest.node;
 
+import com.hiinoono.jaxb.Container;
+import com.hiinoono.jaxb.Node;
 import com.hiinoono.jaxb.Nodes;
 import com.hiinoono.jaxb.Status;
 import com.hiinoono.jaxb.Value;
+import com.hiinoono.jaxb.Manager;
+import com.hiinoono.jaxb.Managers;
 import com.hiinoono.persistence.PersistenceManager;
 import com.hiinoono.rest.auth.HiinoonoRolesAllowed;
 import com.hiinoono.rest.auth.Roles;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -42,13 +48,36 @@ public class NodeResource {
     }
 
 
+    /**
+     * Get all available Nodes in the order of current placement preference.
+     */
     @GET
     @HiinoonoRolesAllowed(roles = {Roles.H_ADMIN},
             message = "You are not permitted to list nodes.")
     public Nodes getNodes() {
         Nodes nodes = new Nodes();
-        nodes.getNodes().addAll(pm.getNodes().collect(Collectors.toList()));
+        List<Node> available = pm.getNodes().collect(Collectors.toList());
+        Collections.sort(available, new NodeComparator(new Container()));
+        nodes.getNodes().addAll(available);
         return nodes;
+    }
+
+
+    /**
+     * Create list of cluster-wide managers and what node is master for each
+     * service/task/etc..
+     */
+    @GET
+    @Path("managers")
+    public Managers managers() {
+
+        List<Manager> _managers = pm.getManagers().collect(Collectors.toList());
+
+        Managers managers = new Managers();
+        managers.getManagers().addAll(_managers);
+
+        return managers;
+
     }
 
 
